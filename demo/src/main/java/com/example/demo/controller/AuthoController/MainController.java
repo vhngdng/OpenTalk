a6a517580f384bc4503bcc6beeb5f501a5e10b07
dung <vhngdng@gmail.com>
2022-11-30 16:07:40 +0700

    fix

--- demo/src/main/java/com/example/demo/controller/AuthoController/MainController.java
+++ demo/src/main/java/com/example/demo/controller/AuthoController/MainController.java
@@ -1,9 +1,6 @@
-package com.example.demo.controller.main;
+package com.example.demo.controller.AuthoController;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MainController {
