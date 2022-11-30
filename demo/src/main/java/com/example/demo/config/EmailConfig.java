932e98fde8bd0a4d8a4b41beacd7c09d44c213ed
dung <vhngdng@gmail.com>
2022-11-30 16:05:24 +0700

    invite open talk

--- demo/src/main/java/com/example/demo/config/EmailConfig.java
+++ demo/src/main/java/com/example/demo/config/EmailConfig.java
@@ -16,87 +16,36 @@ import java.util.Properties;
 //@EnableAutoConfiguration
 public class EmailConfig {
 
-//    private static final String JAVA_MAIL_FILE = "classpath:mail/javamail.properties";
+  private ApplicationContext applicationContext;
+  private Environment environment;
+  @Value("${mail.port}")
+  private int GMAIL_SMTP_PORT;
 
-    private ApplicationContext applicationContext;
-    private Environment environment;
-    @Value("${mail.port}")
-    private int GMAIL_SMTP_PORT;
+  @Value("${mail.host}")
+  private String host;
 
-    @Value("${mail.host}")
-    private String host;
+  @Value("${mail.username}")
+  private String user;
 
-    @Value("${mail.username}")
-    private String user;
+  @Value("${mail.password}")
+  private String password;
 
-    @Value("${mail.password}")
-    private String password;
+  @Value("${mail.debug}")
+  private Boolean debug;
 
-    @Value("${mail.debug}")
-    private Boolean debug;
+  @Bean
+  public JavaMailSender getJavaMailSender() {
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    mailSender.setHost(host);
+    mailSender.setPort(GMAIL_SMTP_PORT);
+    mailSender.setUsername(user);
+    mailSender.setPassword(password);
 
-
-//    @Bean
-//    public JavaMailSender mailSender() throws IOException {
-//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-//        //Basic mail sender configuration, based on emailconfig.properties
-//        mailSender.setHost(host);
-//        mailSender.setPort(GMAIL_SMTP_PORT);
-//
-//        // Set up email config
-//        mailSender.setUsername(user);
-//        mailSender.setPassword(password);
-////
-//        mailSender.setJavaMailProperties(javaMailProperties());
-////        #spring.mail.properties.mail.smtp.ssl.enable=true
-//
-//        return mailSender;
-//    }
-//
-//    @Bean
-//    public Properties javaMailProperties() throws IOException
-//    {
-//        Properties props = new Properties();
-//        props.put("mail.transport.protocol", "smtp");
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.starttls.enable", "true");
-//        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-//        props.put("mail.properties.mail.smtp.ssl.enable", "true");
-//        props.put("mail.debug", debug);
-//
-//        return props;
-//
-//    }
-//
-////    @Primary
-////    @Bean
-////    public FreeMarkerConfigurationFactoryBean factoryBean() {
-////        FreeMarkerConfigurationFactoryBean bean = new FreeMarkerConfigurationFactoryBean();
-////        bean.setTemplateLoaderPath("classpath:/templates");
-////        return bean;
-////    }
-//
-//    @Bean
-//    public Properties factoryBean() throws IOException {
-//        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-//        propertiesFactoryBean.setLocation(new ClassPathResource("mail/emailconfig.properties"));
-//        propertiesFactoryBean.afterPropertiesSet();
-//        return propertiesFactoryBean.getObject();
-//    }
-
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(host);
-        mailSender.setPort(GMAIL_SMTP_PORT);
-        mailSender.setUsername(user);
-        mailSender.setPassword(password);
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-        return mailSender;
-    }
+    Properties props = mailSender.getJavaMailProperties();
+    props.put("mail.transport.protocol", "smtp");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.debug", "true");
+    return mailSender;
+  }
 }
--- demo/src/main/java/com/example/demo/dto/BranchDTO.java
+++ demo/src/main/java/com/example/demo/dto/BranchDTO.java
@@ -1,18 +1,15 @@
 package com.example.demo.dto;
 
-import com.example.demo.entity.OpenTalk;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class BranchDTO {
-    private Long id;
-    private String address;
-    private String name;
+  private Long id;
+  private String address;
+  private String name;
 
 }
